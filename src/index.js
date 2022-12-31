58a0a485eebd442e3c472eb3a61239c2b0545ed4
Madhuri <madhuribansode12@gmail.com>
2022-12-31 15:21:36 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Provider } from "react-redux";
+import store  from "./store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+let persistor = persistStore(store);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+
